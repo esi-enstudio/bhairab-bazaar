53d12ee43f2b72b6d05e3cd1b33904b2e8c98672
Patwary Telecom <sadekinislam6@gmail.com>
2025-11-06 20:37:08 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+// হোমপেজ
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
